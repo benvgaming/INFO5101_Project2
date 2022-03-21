3f698bb6765cf01d477e26b43e09b5f463a59cd0
unknown <benvgaming@pm.me>
2022-03-21 18:07:31 -0400

    Initial Commit

---
+++ Project2_INFO5101/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project2_INFO5101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
